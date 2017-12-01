f30d4550b2a19341f85ae2edafa6a8fcaf29cdfe
JelteMX <jelte.lagendijk@mendix.com>
2017-12-01 08:31:39 +0100

    Add dom methods

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import defineWidget from './helpers/define-widget';
 import { getData, fetchAttr } from './helpers/data';
 import { log, runCallback } from './helpers';
 import { execute, executePromise } from './helpers/microflow';
+import { findWidgetByClass, findWidgetByName} from './helpers/dom';
 
 export {
     defineWidget,
+
     getData,
     fetchAttr,
+
     log,
     runCallback,
+
     execute,
-    executePromise
+    executePromise,
+
+    findWidgetByClass,
+    findWidgetByName,
 }
