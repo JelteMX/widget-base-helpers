a0cf305d433affe4055c7754528375cddd174bce
JelteMX <jelte.lagendijk@mendix.com>
2017-12-12 11:22:48 +0100

    Remove devel:block from defineWidget

--- helpers/define-widget.js
+++ helpers/define-widget.js
@@ -31,6 +31,7 @@ export default function defineWidget(id, template, obj, base, configParam) {
     const widgetObj = obj;
 
     widgetObj._WIDGET_VERSION = version;
+    widgetObj._WIDGET_BASE_ID = `${packageName}.${widgetFolder}.${id}`;
 
     const mixins = [];
     if ('undefined' !== typeof base && null !== base) {
@@ -45,11 +46,6 @@ export default function defineWidget(id, template, obj, base, configParam) {
             widgetObj.templateString = template;
         }
     }
-
-    /* develblock:start */
-    // If the widget is built using Webpack, this block will be removed if you built it using 'yarn run build' or 'npm run build'. This is to make sure someone uses the minified production version.
-    console.warn(`${packageName}.${widgetFolder}.${id} is unminified. Please use the production version`);
-    /* develblock:end */
 
     return declare(`${packageName}.${widgetFolder}.${id}`, mixins, widgetObj);
 }
